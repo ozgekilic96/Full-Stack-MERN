be7a265469bf2f616c069e0decb42c272172e55a
özge kilic <ozgeklc096@gmail.com>
2023-10-13 15:57:54 +0200

    routes, redirect and switch

--- react-frond-end/src/index.js
+++ react-frond-end/src/index.js
@@ -3,5 +3,4 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
