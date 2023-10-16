47ee2a17cf779bb82b9db59d589ee58172e002a3
özge kilic <ozgeklc096@gmail.com>
2023-10-16 17:15:28 +0200

    avatar

--- react-frond-end/src/user/componets/UserItem.js
+++ react-frond-end/src/user/componets/UserItem.js
@@ -1,17 +1,21 @@
 import React from 'react';
+import { Link } from "react-router-dom";
 import './UserItem.css';
+import Avatar from '../../shared/components/Avatar';
 
 const UserItem = props => {
   return (
-    <li> className="user-item"
+    <li className="user-item">
       <div className="user-item__content">
-        <div className="user-item__image">
-         <img src={props.image} alt={props.name} />
-        </div>
-        <div className="user-item__info">
-          <h2>(props.image)</h2>
-          <h3>{props.placeCount} {props.placeCount} ===1 ? 'Place' : 'Places' </h3>
-        </div>
+        <Link to={ `${props.id}/places`}>
+          <div className="user-item__image">
+            <Avatar image={props.image} alt={props.name} />
+          </div>
+          <div className="user-item__info">
+            <h2>{props.name}</h2>
+            <h3>{props.placeCount} {props.placeCount ===1 ? 'Place' : 'Places'} </h3>
+          </div>
+        </Link>
       </div>
     </li>
   )
