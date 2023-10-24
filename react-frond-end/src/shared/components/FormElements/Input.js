4498d3860958c635cdbbec2156428f3316ec68e8
özge kilic <ozgeklc096@gmail.com>
2023-10-24 21:30:12 +0200

    title-descriptino

--- react-frond-end/src/shared/components/FormElements/Input.js
+++ react-frond-end/src/shared/components/FormElements/Input.js
@@ -1,6 +1,6 @@
-import React, { useReducer } from 'react';
+import React, { useReducer, useEffect } from 'react';
 
-import { validate } from '../util/validators'
+import { validate } from '../util/validators';
 import './Input.css';
 
 const inputReducer = (state, action) => {
@@ -29,19 +29,27 @@ const Input = props => {
     isValid: false
   });
 
+  const { id, onInput } = props;
+  const { value, isValid} = inputState
+
+  useEffect(() => {
+    onInput(id, value, isValid)
+  }, [id, value, isValid, onInput])
+
+
   const changeHandler = event => {
     dispatch({
       type: 'CHANGE',
       val: event.target.value,
       validators: props.validators
-     });
+    });
   };
 
   const touchHandler = () => {
     dispatch({
       type: 'TOUCH'
     });
-  }
+  };
 
   const element =
     props.element === 'input' ? (
@@ -58,6 +66,7 @@ const Input = props => {
         id={props.id}
         rows={props.rows || 3}
         onChange={changeHandler}
+        onBlur={touchHandler}
         value={inputState.value}
       />
     );
