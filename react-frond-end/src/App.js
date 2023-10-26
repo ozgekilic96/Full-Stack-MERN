dfaffb3a043823da15d002c47235aa1dba13727c
özge kilic <ozgeklc096@gmail.com>
2023-10-26 20:28:09 +0200

    update place

--- react-frond-end/src/App.js
+++ react-frond-end/src/App.js
@@ -8,6 +8,7 @@ import {
 
 import Users from './user/pages/Users';
 import NewPlace from './places/pages/NewPlace';
+import UpdatePlace from './places/pages/UpdatePlace'
 import UserPlaces from './places/pages/UserPlaces';
 import MainNavigation from './shared/components/Navigation/MainNavigaton';
 
@@ -25,6 +26,9 @@ const App = () => {
           </Route>
           <Route path="/places/new" exact>
             <NewPlace />
+          </Route>
+          <Route path="/places/:placesId" exact >
+            <UpdatePlace />
           </Route>
           <Redirect to="/" />
         </Switch>
