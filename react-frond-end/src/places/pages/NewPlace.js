e8bbedd6c92bf60718cf42e939d1d6392cc0197a
özge kilic <ozgeklc096@gmail.com>
2023-10-26 12:16:06 +0200

    address on the form

--- react-frond-end/src/places/pages/NewPlace.js
+++ react-frond-end/src/places/pages/NewPlace.js
@@ -56,8 +56,14 @@ const NewPlace = () => {
     });
   }, []);
 
+  const placeSubmitHandler = event => {
+    event.preventDefault();
+    console.log(formState.inputs); // send it to backend later
+  }
+
+
   return (
-    <form className="place-form">
+    <form className="place-form" onSubmit={placeSubmitHandler}>
       <Input
         id="title"
         element="input"
@@ -74,6 +80,14 @@ const NewPlace = () => {
         validators={[VALIDATOR_MINLENGTH(5)]}
         errorText="Please enter a valid description (at least 5 characters)."
         onInput={inputHandler}
+      />
+      <Input
+      id="address"
+      element="input"
+      label="Address"
+      validators={[VALIDATOR_REQUIRE()]}
+      errorText="Please enter a valid address)."
+      onInput={inputHandler}
       />
       <Button type="submit" disabled={!formState.isValid}>
         ADD PLACE
