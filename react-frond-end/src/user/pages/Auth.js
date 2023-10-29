972d4212912bb448134f9d88f020d435d2312542
özge kilic <ozgeklc096@gmail.com>
2023-10-29 20:57:20 +0100

    autherizatio

---
+++ react-frond-end/src/user/pages/Auth.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import Button from '../../shared/components/FormElements/Button'
+import Input from '../../shared/components/FormElements/Input'
+import Card from '../../shared/components/UIElements/Card'
+import './Auth.css';
+
+const Auth = () => {
+  return <Card>
+    <h2> Login Required </h2>
+    <form> </form>
+    </Card>
+
+}
+
+export default Auth;
