701ab20da798ce226a0ea0fe11570bcad4768db1
özge kilic <ozgeklc096@gmail.com>
2023-11-10 16:48:51 +0100

    connect frondend-backend

--- Node-express-backend/app.js
+++ Node-express-backend/app.js
@@ -8,9 +8,40 @@ const HttpError = require('./models/http-error');
 
 const app = express();
 
+/* const { MONGO_URI } = process.env;
+
+exports.connect = () => {
+  mongoose
+    .connect(MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      // useCreateIndex: true,
+      // useFindAndModify: false,
+    })
+    .then(() => {
+      console.log("Conneccion a BD exitosa");
+    })
+    .catch((err) => {
+      console.log("Coneccion a BD fallida...");
+      console.error(err);
+      process.exit(1);
+    });
+};
+ */
 app.use(bodyParser.json());
 
-app.use('/api/places', placesRoutes); 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  );
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
+
+  next();
+});
+
+app.use('/api/places', placesRoutes);
 app.use('/api/users', usersRoutes);
 
 app.use((req, res, next) => {
@@ -26,11 +57,34 @@ app.use((error, req, res, next) => {
   res.json({message: error.message || 'An unknown error occurred!'});
 });
 
-mongoose
-  .connect('mongodb+srv://<user_name>:<password>@bcex.snckbaj.mongodb.net/?retryWrites=true&w=majority')
+/* mongoose
+  .connect('mongodb+srv://ozgekilic96:ozge1234@bcex.snckbaj.mongodb.net/?retryWrites=true&w=majority')
   .then(() => {
     app.listen(5000);
   })
   .catch(err => {
     console.log(err);
   });
+ */
+
+
+const { MONGO_URI } = process.env;
+
+exports.connect = () => {
+  mongoose
+    .connect('mongodb+srv://ozgekilic96:<password>@bcex.snckbaj.mongodb.net/mern?retryWrites=true&w=majority', {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      // useCreateIndex: true,
+      // useFindAndModify: false,
+    })
+    .then(() => {
+      app.listen(5000);
+      console.log("Conneccion a BD exitosa");
+    })
+    .catch((err) => {
+      console.log("Coneccion a BD fallida...");
+      console.error(err);
+      process.exit(1);
+    });
+};
